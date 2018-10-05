4b9fde3208dea14f1c70f7bc35bec8b64127d45d
Jack Ducham <jackducham@gmail.com>
2018-10-05 12:34:55 -0500

    added testbench

--- datapath.sv
+++ datapath.sv
@@ -12,10 +12,10 @@ module datapath
 	assign MDR = c;
 	mux2 mux2mdr(.d0(a),.d1(c),.s(GATEMDR),.y(PC_MDR));
 	mux2 mux2pc(.d0(16'bZZZZZZZZZZZZZZZZ),.d1(b),.s(GATEPC),.y(a));
-	register16 pc_reg(.Clk(Clk),.Reset(Reset),.Load(LD_PC),.D(e),.Data_Out(b));
+	register16 pc_reg(.Clk(Clk),.Reset(Reset),.Load(LD_PC),.D(d),.Data_Out(b));
 	register16 mdr_reg(.Clk(Clk),.Reset(Reset),.Load(LD_MDR),.D(f),.Data_Out(c));
 	plus_1 pc_plus_1(.pc(b),.y(d));
-	mux2 pcmux(.d0(DATA),.d1(d),.s(PCMUX_EN),.y(e));
+	//mux2 pcmux(.d0(DATA),.d1(d),.s(PCMUX_EN),.y(e));
 	mux2 mdrmux(.d0(DATA),.d1(DATA_TO_MDR),.s(MIO_EN),.y(f));
 	register16 ir_reg(.Clk(Clk),.Reset(Reset),.Load(LD_IR),.D(DATA),.Data_Out(IR));
 	register16 mar_reg(.Clk(Clk),.Reset(Reset),.Load(LD_MAR),.D(DATA),.Data_Out(MAR));
---
+++ testbench.sv
@@ -0,0 +1,65 @@
+module testbench();
+
+timeunit 10ns;
+timeprecision 1ns;
+
+// internal signals
+logic [15:0] S;
+logic Clk = 0;
+logic Reset, Run, Continue;
+logic [11:0] LED;
+logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7;
+logic CE, UB, LB, OE, WE;
+logic [19:0] ADDR;
+wire [15:0] Data;
+
+// internal monitoring
+logic [15:0] MAR0, MDR0, IR0, PC0, PC;
+
+integer error_count = 0;
+
+lab6_toplevel p0(.*);
+
+always #1 Clk = ~Clk;
+
+always_comb begin
+    MAR0 = p0.my_slc.d0.MAR;
+    MDR0 = p0.my_slc.d0.MDR;
+    IR0 = p0.my_slc.d0.IR;
+    PC0 = p0.my_slc.d0.PC_MDR;
+    PC = p0.my_slc.d0.b;
+end
+
+
+initial begin
+    Reset = 1;
+    Run = 1;
+    Continue = 1;
+    S = 16'hFFFF;
+    
+    #5 Reset = 0;
+    #5 Reset = 1;
+    #5 Run = 0;
+    #5 Run = 1;
+    #5 Continue = 0;
+    #5 Continue = 1;
+    #5 Run = 0;
+    #5 Run = 1;
+    #5 Continue = 0;
+    #5 Continue = 1;
+    #5 Run = 0;
+    #5 Run = 1;
+    #5 Continue = 0;
+    #5 Continue = 1;
+    #5 Run = 0;
+    #5 Run = 1;
+    #5 Continue = 0;
+    #5 Continue = 1;
+    #5 Run = 0;
+    #5 Run = 1;
+    #5 Continue = 0;
+    #5 Continue = 1;
+        
+    
+end
+endmodule
